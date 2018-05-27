a2f999510075938fa1706d72f6f07df868d371fa
garyholland <gholland@dataexpertsgroup.com>
2018-05-27 20:36:52 +1000

    first commit

---
+++ src/dexih.proxy/Services/IStreams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using dexih.proxy.Models;
+using Microsoft.CodeAnalysis;
+
+namespace dexih.proxy.Services
+{
+    public interface IStreams
+    {
+        void SetDownloadStream(DownloadObject downloadObject);
+        DownloadObject GetDownloadStream(string key, string securityKey);
+        void RemoveDownloadStream(string key);
+    }
+}

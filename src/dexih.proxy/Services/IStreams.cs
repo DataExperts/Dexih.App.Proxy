24e04a0c12a729b46abe61e5ed4180f3177686c9
garyholland <gholland@dataexpertsgroup.com>
2018-06-15 21:05:49 +1000

    refactor

--- src/dexih.proxy/Services/IStreams.cs
+++ src/dexih.proxy/Services/IStreams.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Threading.Tasks;
-using dexih.proxy.Models;
-using Microsoft.CodeAnalysis;
+﻿using dexih.proxy.Models;
 
 namespace dexih.proxy.Services
 {
--- src/dexih.proxy/Services/Streams.cs
+++ src/dexih.proxy/Services/Streams.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.IO;
-using System.Threading.Tasks;
 using System.Timers;
 using dexih.proxy.Models;
 
